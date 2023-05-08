dc6c73efc6ee0f556bf7f59e7b5a89cd33032ed1
Timo <tibue69@gmail.com>
2023-05-08 21:02:59 +0200

    Bump version to 0.1.5

--- src/ezcord/__init__.py
+++ src/ezcord/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "ezcord"
 __author__ = "tibue99"
 __license__ = "MIT"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from . import components, emb, logs, times, utils
 from .bot import Bot, PrefixBot

a5a6d7ba10256d7a489fe5fd76b7d9e7036f7a50
Timo <35654063+tibue99@users.noreply.github.com>
2023-06-17 20:04:06 +0200

    Fix database connections (#5)
    * Fix unclosed db connections
    
    * Add method for transactions
    
    * Fix start method

--- src/ezcord/sql.py
+++ src/ezcord/sql.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from copy import deepcopy
+
 import aiosqlite
 
 
@@ -11,7 +13,8 @@ class DBHandler:
     path:
         The path to the database file.
     transaction:
-        Automatically create a new connection that will be used for all transaction queries.
+        Automatically create a new connection that will be used for all queries.
+        A transaction must be closed with :meth:`close` or by using ``end=True`` in :meth:`exec`.
     connection:
         A connection to the database. If not provided, a new connection will be created.
         If ``transaction`` is ``True``, this will be ignored.
@@ -39,6 +42,14 @@ class DBHandler:
         if len(args) == 1 and isinstance(args, tuple):
             return args[0]
         return args
+
+    def start(self):
+        """Returns an instance of :class:`.DBHandler` with the current settings
+        and ``transaction=True``.
+        """
+        cls = deepcopy(self)
+        cls.transaction = True
+        return cls
 
     async def _connect(self, **kwargs) -> aiosqlite.Connection:
         """Connect to an SQLite database. This is useful for transactions."""
@@ -82,12 +93,14 @@ class DBHandler:
         db = await self._connect(**kwargs)
         async with db.execute(sql, args) as cursor:
             result = await cursor.fetchone()
+        if not self.connection:
+            await db.close()
+
         if result is None:
             return None
         if len(result) == 1:
             return result[0]
-        if not self.connection:
-            await db.close()
+
         return result
 
     async def all(self, sql: str, *args, **kwargs) -> list:
@@ -112,10 +125,11 @@ class DBHandler:
         db = await self._connect(**kwargs)
         async with db.execute(sql, args) as cursor:
             result = await cursor.fetchall()
-        if len(result) == 0 or len(result[0]) == 1:
-            return [row[0] for row in result]
         if not self.connection:
             await db.close()
+        if len(result) == 0 or len(result[0]) == 1:
+            return [row[0] for row in result]
+
         return result
 
     async def exec(self, sql: str, *args, end: bool = False, **kwargs) -> None:
@@ -127,6 +141,7 @@ class DBHandler:
             The SQL query to execute.
         end:
             Whether to commit and close the connection after executing the query.
+            This is only needed for transactions.
         *args:
             Arguments for the query.
         **kwargs:

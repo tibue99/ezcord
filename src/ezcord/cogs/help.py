3f6e57306c2825998868a316f242b0bdd04a2753
ryzm <contact@ryzm.tech>
2023-06-18 15:44:15 +0200

    Update help.py

--- src/ezcord/cogs/help.py
+++ src/ezcord/cogs/help.py
@@ -10,7 +10,7 @@ from ..components import EzView
 from ..internal import t
 
 
-def get_emoji(cog: Cog) -> str:
+def get_emoji(cog):
     if hasattr(cog, "emoji") and cog.emoji:
         emoji = cog.emoji
     else:
@@ -19,44 +19,67 @@ def get_emoji(cog: Cog) -> str:
     return emoji
 
 
-def get_group(cog: Cog) -> str | None:
-    if hasattr(cog, "group") and cog.group:
-        return cog.group
-    return None
-
-
-class Help(Cog, hidden=True):
-    @slash_command(name="help", description=t("cmd_description"))
+class Help(Cog, command_attrs=dict(hidden=True)):
+    @slash_command(name="help", description=t("cmd_description"), hidden=True)
     async def help(self, ctx):
         embed = self.bot.help["embed"]
         if embed is None:
             embed = discord.Embed(title=t("embed_title"), color=discord.Color.blue())
 
         options = []
-        commands = {}
-        for name, cog in self.bot.cogs.items():
-            if hasattr(cog, "hidden") and cog.hidden:
-                continue
 
-            group = get_group(cog)
-            name = group if group else name
+        for name, cog in zip(self.bot.cogs.keys(), self.bot.cogs.values()):
             name = name.title()
-            if name not in commands:
-                commands[name] = {}
-            if "cmds" not in commands[name]:
-                commands[name]["cmds"] = []
 
-            if len(cog.get_commands()) == 0:
+            if len(cog.get_commands()) == 0 or cog == self:
                 continue
 
             emoji = get_emoji(cog)
             emoji_str = f"`{emoji}` ・ "
-            commands[name]["emoji"] = emoji
 
             desc = cog.description
             if not cog.description:
                 desc = t("default_description", name)
 
+            embed.add_field(name=f"{emoji_str or ''}{name}", value=desc, inline=False)
+
+            option = discord.SelectOption(label=name, value=name, emoji=emoji)
+            options.append(option)
+
+        view = View(options, self.bot, ctx.user)
+        await ctx.respond(view=view, embed=embed, ephemeral=self.bot.help["ephemeral"])
+
+
+def setup(bot: Bot):
+    bot.add_cog(Help(bot))
+
+
+class Select(discord.ui.Select):
+    def __init__(
+        self, options: list[discord.SelectOption], bot: Bot, member: discord.Member | discord.User
+    ):
+        super().__init__(min_values=1, max_values=1, placeholder=t("placeholder"), options=options)
+        self.bot = bot
+        self.member = member
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user != self.member:
+            return await interaction.response.send_message(t("wrong_user"), ephemeral=True)
+
+        embed = None
+        for name, cog in zip(self.bot.cogs.keys(), self.bot.cogs.values()):
+            if self.values[0] != name:
+                continue
+
+            title = name.title()
+            emoji = get_emoji(cog)
+
+            embed = self.bot.help["embed"]
+            if embed is None:
+                embed = discord.Embed(
+                    title=f"`{emoji}` - {title}",
+                    color=discord.Color.blue(),
+                )
             for command in cog.walk_commands():
                 if type(command) in [
                     discord.MessageCommand,
@@ -65,67 +88,26 @@ class Help(Cog, hidden=True):
                 ]:
                     continue
 
-                commands[name]["cmds"].append(command)
+                embed.add_field(
+                    name=f"{command.mention}",
+                    value=f"`{command.description}`",
+                    inline=False,
+                )
 
-            if not group:
-                embed.add_field(name=f"{emoji_str or ''}{name}", value=desc, inline=False)
-                option = discord.SelectOption(label=name, emoji=emoji)
-                options.append(option)
+                if embed.fields > 25:
+                    print("Too many fields") # Could be replaced by the logger
+                    break
 
-        view = CategoryView(options, self.bot, ctx.user, commands)
-        await ctx.respond(view=view, embed=embed, ephemeral=self.bot.help["ephemeral"])
-
-
-def setup(bot: Bot):
-    bot.add_cog(Help(bot))
-
-
-class CategorySelect(discord.ui.Select):
-    def __init__(
-        self,
-        options: list[discord.SelectOption],
-        bot: Bot,
-        member: discord.Member | discord.User,
-        commands: dict[str, dict],
-    ):
-        super().__init__(min_values=1, max_values=1, placeholder=t("placeholder"), options=options)
-        self.bot = bot
-        self.member = member
-        self.commands = commands
-
-    async def callback(self, interaction: discord.Interaction):
-        if interaction.user != self.member:
-            return await interaction.response.send_message(t("wrong_user"), ephemeral=True)
-
-        cmds = self.commands[self.values[0]]
-        title = self.values[0].title()
-        emoji = cmds["emoji"]
-
-        embed = self.bot.help["embed"]
-        if embed is None:
-            embed = discord.Embed(
-                title=f"`{emoji}` - {title}",
-                color=discord.Color.blue(),
-            )
-        for command in cmds["cmds"]:
-            embed.add_field(
-                name=f"{command.mention}",
-                value=f"`{command.description}`",
-                inline=False,
-            )
 
         await interaction.response.edit_message(
-            embed=embed, view=CategoryView(self.options, self.bot, self.member, self.commands)
+            embed=embed, view=View(self.options, self.bot, self.member)
         )
 
 
-class CategoryView(EzView):
+class View(EzView):
     def __init__(
-        self,
-        options: list[discord.SelectOption],
-        bot: Bot,
-        member: discord.Member | discord.User,
-        commands: dict[str, dict],
+        self, options: list[discord.SelectOption], bot: Bot, member: discord.Member | discord.User
     ):
         super().__init__(timeout=500, disable_on_timeout=True)
-        self.add_item(CategorySelect(options, bot, member, commands))
+        self.add_item(Select(options, bot, member))
+

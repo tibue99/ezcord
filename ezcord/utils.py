8fb9dcde11134a25f91caf9e189b25373d665aca
Timo <tibue69@gmail.com>
2023-08-26 23:03:43 +0200

    Add ez_autocomplete

--- ezcord/utils.py
+++ ezcord/utils.py
@@ -1,7 +1,12 @@
 """Some utility functions for Pycord and Python."""
 
+import asyncio
 import io
+import itertools
 import json
+from typing import Any
+
+from discord.utils import AutocompleteFunc, V, Values
 
 from .internal.dc import discord
 
@@ -66,3 +71,33 @@ def create_html_file(html: str, filename: str = "data.html", **kwargs) -> discor
     :class:`discord.File`
     """
     return create_text_file(html, filename, **kwargs)
+
+
+def ez_autocomplete(values: Values) -> AutocompleteFunc:
+    """A similar function as :func:`~discord.utils.basic_autocomplete`, but instead of returning
+    options starting with the user's value, it returns options containing the user's value.
+
+    Parameters
+    ----------
+    values: Iterable[str]
+        Accepts an iterable of :class:`str`, a callable (sync or async) that takes a single argument
+        of :class:`~discord.AutocompleteContext`, or a coroutine. Must resolve to an iterable of :class:`str`.
+
+    """
+
+    async def autocomplete_callback(ctx: discord.AutocompleteContext) -> V:
+        _values = values
+
+        if callable(_values):
+            _values = _values(ctx)
+        if asyncio.iscoroutine(_values):
+            _values = await _values
+
+        def check(item: Any) -> bool:
+            item = getattr(item, "name", item)
+            return str(ctx.value or "").lower() in str(item).lower()
+
+        gen = (val for val in _values if check(val))
+        return iter(itertools.islice(gen, 25))
+
+    return autocomplete_callback

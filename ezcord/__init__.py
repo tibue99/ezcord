7e5b760d5ba51e1e7760507ca3161e6c8554c455
Timo <35654063+tibue99@users.noreply.github.com>
2024-01-04 23:41:07 +0100

    Bump version to 0.5.2

--- ezcord/__init__.py
+++ ezcord/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "ezcord"
 __author__ = "tibue99"
 __license__ = "MIT"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from . import blacklist, components, emb, logs, sql, times, utils
 from .bot import *

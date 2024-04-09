d68021cccc9ed9190742127d125f0754cadc3893
Timo <35654063+tibue99@users.noreply.github.com>
2024-04-09 12:20:39 +0200

    Remove connection

--- ezcord/sql/postgresql.py
+++ ezcord/sql/postgresql.py
@@ -21,9 +21,6 @@ class PGHandler:
         The user of the database.
     password:
         The password of the database.
-    connection:
-        A connection to the database. If not provided, a new connection will be created.
-        If ``auto_connect`` is ``True``, this will be ignored.
     auto_setup:
         Whether to call :meth:`setup` when the first instance of this class is created. Defaults to ``True``.
         This is called in the ``on_ready`` event of the bot.
@@ -45,29 +42,21 @@ class PGHandler:
         dbname: str,
         user: str,
         password: str,
-        connection: asyncpg.Connection | None = None,
         auto_setup: bool = True,
-        retries: int = 2,
         **kwargs,
     ):
-        self.connection = connection
-        self.retries = retries
         self.host = host
         self.port = port
         self.dbname = dbname
         self.user = user
         self.password = password
-        self.connection = connection
         self.kwargs = kwargs
 
         if auto_setup and self not in self._auto_setup:
             PGHandler._auto_setup.append(self)
 
     async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        return await self.close()
+        return self.pool or await self._check_pool()
 
     def _process_args(self, args) -> tuple:
         """If SQL query parameters are passed as a tuple instead of single values,
@@ -82,7 +71,7 @@ class PGHandler:
 
         return args
 
-    async def _check_pool(self, **kwargs) -> asyncpg.Pool:
+    async def _check_pool(self) -> asyncpg.Pool:
         """Create a new connection pool. If the class instance has an active connection,
         that connection will be returned instead.
         """
@@ -90,7 +79,6 @@ class PGHandler:
         if self.pool is not None:
             return self.pool
 
-        con_args = {**kwargs, **self.kwargs}
         self.pool = await asyncpg.create_pool(
             host=self.host,
             port=self.port,
@@ -98,42 +86,9 @@ class PGHandler:
             user=self.user,
             password=self.password,
             command_timeout=30,
-            **con_args,
+            **self.kwargs,
         )
-
         return self.pool
-
-    async def _connect(self, **kwargs) -> asyncpg.Connection:
-        """Connect to a database. If the class instance has an active connection,
-        that connection will be returned instead.
-        """
-
-        if self.connection is not None:
-            return self.connection
-
-        con_args = {**kwargs, **self.kwargs}
-        self.connection = await asyncpg.connect(
-            host=self.host,
-            port=self.port,
-            database=self.dbname,
-            user=self.user,
-            password=self.password,
-            **con_args,
-        )
-
-        return self.connection
-
-    async def close(self):
-        """Commits and closes the current connection to the database.
-
-        This is called automatically when using :meth:`start` as a context manager.
-        """
-        if self.connection is not None:
-            await self.connection.close()
-
-    async def _close(self, db: asyncpg.Connection):
-        if not self.connection:
-            await db.close()
 
     async def one(self, sql: str, *args, **kwargs) -> asyncpg.Record | None:
         """Returns one result row. If no row is found, ``None`` is returned.
@@ -147,22 +102,17 @@ class PGHandler:
         *args:
             Arguments for the query.
         **kwargs:
-            Keyword arguments for the connection.
+            Keyword arguments for the query.
 
         Returns
         -------
         The result row or ``None``. A result row is either a tuple or a single value.
         """
         args = self._process_args(args)
-        pool = await self._check_pool(**kwargs)
+        pool = await self._check_pool()
 
         async with pool.acquire() as con:
-            result = await con.fetchrow(sql, *args)
-
-        # if len(result) == 1:
-        #     return result[0]
-
-        return result
+            return await con.fetchrow(sql, *args, **kwargs)
 
     async def all(self, sql: str, *args, **kwargs) -> list:
         """Returns all result rows.
@@ -176,22 +126,17 @@ class PGHandler:
         *args:
             Arguments for the query.
         **kwargs:
-            Keyword arguments for the connection.
+            Keyword arguments for the query.
 
         Returns
         -------
         A list of result rows. A result row is either a tuple or a single value.
         """
         args = self._process_args(args)
-        pool = await self._check_pool(**kwargs)
+        pool = await self._check_pool()
 
         async with pool.acquire() as con:
-            result = await con.fetch(sql, *args)
-
-        # if len(result) == 0 or len(result[0]) == 1:
-        #     return [row[0] for row in result]
-
-        return result
+            return await con.fetch(sql, *args, **kwargs)
 
     async def exec(self, sql: str, *args, **kwargs) -> str:
         """Executes a SQL query.
@@ -203,13 +148,13 @@ class PGHandler:
         *args:
             Arguments for the query.
         **kwargs:
-            Keyword arguments for the connection.
+            Keyword arguments for the query.
         """
         args = self._process_args(args)
-        pool = await self._check_pool(**kwargs)
+        pool = await self._check_pool()
 
         async with pool.acquire() as con:
-            return await con.execute(sql, *args)
+            return await con.execute(sql, *args, **kwargs)
 
     async def execute(self, sql: str, *args, **kwargs) -> str:
         """Alias for :meth:`exec`."""
@@ -225,9 +170,9 @@ class PGHandler:
         *args:
             Arguments for the mutliquery.
         **kwargs:
-            Keyword arguments for the connection.
+            Keyword arguments for the query.
         """
-        pool = await self._check_pool(**kwargs)
+        pool = await self._check_pool()
 
         async with pool.acquire() as con:
-            return await con.executemany(sql, args)
+            return await con.executemany(sql, *args, **kwargs)
